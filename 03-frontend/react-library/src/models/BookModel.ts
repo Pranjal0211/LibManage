1b1e55e07725cd781fc80aa9e4c4f69557dc5f7e
Pranjal0211 <pranjalsharma0211@gmail.com>
2024-10-07 11:00:00 -0700

    Added frontend Models folder

---
+++ 03-frontend/react-library/src/models/BookModel.ts
@@ -0,0 +1,27 @@
+class BookModel {
+
+    //varibales
+    id: number;
+    title: string;
+    author?: string;
+    description?: string;
+    copies?: number;
+    copiesAvailable?: number;
+    category?: string;
+    img?: string;
+
+    //constructor 
+    constructor (id: number, title: string, author: string, description: string, 
+        copies: number, copiesAvailable: number, category: string, img: string){
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.description = description;
+            this.copies = copies;
+            this.copiesAvailable = copiesAvailable;
+            this.category = category;
+            this.img = img;
+        }
+}
+
+export default BookModel;
